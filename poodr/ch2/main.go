0122d7b3192c7998970c6aa95a3969116a59ab09
Matt Reath <mattreath@icloud.com>
2023-07-03 09:29:58 -0500

    updated ch2 output

--- poodr/ch2/main.go
+++ poodr/ch2/main.go
@@ -1,10 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	wheel := NewWheel(26, 1.5)
-	println(wheel.Circumference())
+	fmt.Printf("Circumference:\t%f\n", wheel.Circumference())
 
 	gear := NewGear(52, 11, wheel)
-	println(gear.GearInches())
-	println(gear.Ratio())
+	fmt.Printf("Gear Inches:\t%f\n", gear.GearInches())
+	fmt.Printf("Gear Ratio:\t%f\n", gear.Ratio())
 }
